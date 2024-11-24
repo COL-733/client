ec426f6a3db4f848b5d53dba87697772d33760b0
Gaurav <gaurav03.iitd@gmail.com>
2024-11-24 17:18:57 +0530

    bugs

--- client.py
+++ client.py
@@ -12,18 +12,55 @@ import log
 import json
 
 class Client:
-    def __init__(self, server_host: str, server_port: int):
+    def __init__(self, server_host: str, server_port: int, client_name : str):
         self.server_host = server_host
         self.server_port = server_port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.cv = Condition()
-        self.list_res: list[set[VersionedValue]] = []
+        self.list_res: list[VersionedValue] = []
         self.to_reconcile_context = []
         self.current_msg_id = None
         self.current_source = None
         self.current_dest = None
         self.current_key = None
         self.current_request_type = None
+        self.final_read_ans = None
+        self.client_name = client_name 
+        self.connect()
+        self.run()
+
+    def deserialize_res(self, serialized: list[list]) -> list[VersionedValue]:
+            """Deserialize a list of lists into a set of VersionedValue objects."""
+            deserialized = []
+
+            if (not len(serialized[0]) == len(serialized[1])) and len(serialized[0])==1:
+
+                new_vc = VectorClock()
+                for vc in serialized[1]:
+                    new_vc = new_vc.merge(vc)
+                
+                deserialized.append(VersionedValue(serialized[0],new_vc))
+            
+            else:
+
+                for i in range(0,len(serialized[0])):
+                    deserialized.append(VersionedValue(serialized[0][i], VectorClock(serialized[1][i])))
+
+            return deserialized
+    def connect(self):
+        try:
+            self.socket.connect((self.server_host, self.server_port))
+            logging.info(f"Connected to load balancer at {self.server_host}:{self.server_port}")
+        except ConnectionRefusedError:
+            logging.error("Failed to connect to load balancer")
+            raise
+            
+            
+    def close(self):
+        self.socket.close()
+    def run(self):
+        receiver_thread = Thread(target=self.receive_from_switch)
+        receiver_thread.start()
 
     def string_add(self, json_str1, json_str2):
         dict1 = json.loads(json_str1)
@@ -63,12 +100,12 @@ class Client:
                 combined_value = vv.value
         return combined_value
 
-    def reconcile_and_send(self):
+    def reconcile_and_send(self,i):
         """Reconcile versioned values and send appropriate request"""
         with self.cv:
             if len(self.list_res) == 0:
                 return True
-            elif len(self.list_res) == 1 and self.current_request_type == MessageType.GET:
+            elif len(self.list_res) == 1 and self.current_request_type == MessageType.GET_RES:
                 return True
             else:
                 all_values = set()
@@ -78,16 +115,18 @@ class Client:
                 merged_clock = self.compute_vector_clock(all_values)
                 reconciled_value = self.reconcile_values(all_values)
 
-                if self.current_request_type == MessageType.GET:
+                if i==0 :
+                    self.final_read_ans = reconciled_value
                     message = Message(
                         self.current_msg_id,
-                        MessageType.GET,
+                        MessageType.PUT,
                         self.current_source,
                         self.current_dest,
                         key=self.current_key,
                         context=merged_clock.clock,
                         value=reconciled_value
                     )
+                    
                 else:  # PUT
                     message = Message(
                         self.current_msg_id,
@@ -109,13 +148,11 @@ class Client:
 
     def handle_switch(self, msg: Message):
         with self.cv:
-            if msg.msg_type in {MessageType.GET, MessageType.PUT}:
-                self.list_res.append(msg.kwargs.get("context", set()))
+            if msg.msg_type in {MessageType.GET_RES, MessageType.PUT_ACK}:
+                res = self.deserialize_res(msg.kwargs["res"])
+                self.list_res = res
                 self.cv.notify()
-                return 0
-            elif msg.msg_type in {MessageType.GET_RES, MessageType.PUT_ACK}:
-                self.cv.notify()
-                return msg.kwargs.get("res")
+                return True
 
     def receive_from_switch(self) -> None:
         """Thread to receive messages from Switch."""
@@ -145,8 +182,12 @@ class Client:
                 self.socket.send(message.serialize())
                 logging.info(f"Sent GET message: {message}")
                 self.cv.wait()
+                if self.final_read_ans :
+                    logging.info(f"Final GET answer {self.final_read_ans}")
+                # if there is some listres
                 if self.list_res:
-                    return self.reconcile_and_send()
+                    x =self.reconcile_and_send(0)
+                    return self.list_res
                 return True
             except Exception as e:
                 logging.error(f"Failed to send GET message: {e}")
@@ -158,16 +199,17 @@ class Client:
             self.current_source = source
             self.current_dest = destination
             self.current_key = key
-            self.current_request_type = MessageType.PUT
+            self.current_request_type = MessageType.GET
             self.list_res = []
 
-            message = Message(msg_id, MessageType.PUT, source, destination, key=key, value=value)
+            message = Message(msg_id, MessageType.GET, source, destination, key=key, value=value)
             try:
                 self.socket.send(message.serialize())
                 logging.info(f"Sent PUT message: {message}")
                 self.cv.wait()
+                # if the there is some list_res
                 if self.list_res:
-                    return self.reconcile_and_send()
+                    return self.reconcile_and_send(1)
                 return True
             except Exception as e:
                 logging.error(f"Failed to send PUT message: {e}")
@@ -177,7 +219,6 @@ def test_request():
     client = Client('localhost', 4000)
     
     try:
-        client.connect()
         
         get_test_cases = [
             ("msg1", "client1", "server1", "key1"),
@@ -205,7 +246,6 @@ def test_request():
 def handle_interactive_mode():
     client = Client('localhost', 4000)
     try:
-        client.connect()
         
         while True:
             try:
